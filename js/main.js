f07ef9f5fdf751a1f8e5f84e90068aeaefb03362
Nadiia Kozhushko <kozhushko51@gmail.com>
2019-12-26 13:16:17 +0200

    intro-slider section creating

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.intro-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	speed: 500,
+	fade: true,
+	dots: true,
+	arrows: false,
+	asNavFor: '.slider-preview'
+});
+$('.slider-preview').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.intro-slider',
+	arrows: true,
+	prevArrow:
+		'<button type="button" class="slider-preview__button preview-prev">&#xf177;</button>',
+	nextArrow:
+		'<button type="button" class="slider-preview__button preview-next">&#xf178;</button>'
+});
