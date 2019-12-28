4305c4388b19914ee15444f366a6c1f7d75e3915
Nadiia Kozhushko <kozhushko51@gmail.com>
2019-12-28 17:55:55 +0200

    create section welcome with travel-slider

--- js/main.js
+++ js/main.js
@@ -14,8 +14,19 @@ $('.slider-preview').slick({
 	slidesToScroll: 1,
 	asNavFor: '.intro-slider',
 	arrows: true,
+
 	prevArrow:
 		'<button type="button" class="slider-preview__button preview-prev">&#xf177;</button>',
 	nextArrow:
 		'<button type="button" class="slider-preview__button preview-next">&#xf178;</button>'
+});
+$('.travel-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	prevArrow:
+		'<button type="button" class="travel-slider__button travel-prev">&larr;</button>',
+	nextArrow:
+		'<button type="button" class="travel-slider__button travel-next">&rarr;</button>'
 });
